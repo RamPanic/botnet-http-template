8ca9366c6d08d8e328320aba3d3ca56bbdb3c4fe
RamPanic <67488206+RamPanic@users.noreply.github.com>
2022-01-24 00:21:58 -0300

    Configuración y runserver actualizado

--- config.py
+++ config.py
@@ -1,19 +1,64 @@
-# General
 
-IP_SERVER = "127.0.0.1"
-PORT_SERVER = 5000
+"""Flask configuration."""
 
-SECRET_KEY = "test"
+class Config:
 
-# Database
+    """Base config."""
 
-MYSQL_USER = "root"
-MYSQL_PASSWORD = ""
-MYSQL_DATABASE = "botnet"
-MYSQL_PORT = 3306
+    SECRET_KEY = ""
+    
+    STATIC_FOLDER = 'static'
+    
+    TEMPLATES_FOLDER = 'templates'
 
-DATABASE_URI = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{IP_SERVER}/{MYSQL_DATABASE}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-# Debugging
 
-DEBUG = True
+class ProdConfig(Config):
+
+    # Basic
+
+    IP_SERVER = ""
+    PORT_SERVER = 5000
+
+    ENV = 'production'
+    DEBUG = False
+    TESTING = False
+
+    # Database
+
+    MYSQL_USER = ""
+    MYSQL_PASSWORD = ""
+    MYSQL_DATABASE = ""
+    MYSQL_PORT = 3306
+
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{IP_SERVER}/{MYSQL_DATABASE}"
+
+
+class DevConfig(Config):
+
+    # Basic
+
+    HOST = "127.0.0.1"
+    PORT = 5000
+
+    ENV = 'development'
+    DEBUG = True
+    TESTING = True
+
+    # Database
+
+    MYSQL_USER = "root"
+    MYSQL_PASSWORD = ""
+    MYSQL_DATABASE = "botnet"
+    MYSQL_PORT = 3306
+
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{HOST}/{MYSQL_DATABASE}"
+
+
+config = {
+ 
+    'dev': DevConfig,
+    'prod': ProdConfig
+
+}
--- runserver.py
+++ runserver.py
@@ -1,8 +1,7 @@
 
-import config
-
 from flask import Flask
 
+from config import config
 from routes.bots import bots
 from routes.general import general
 from schemas.schema import marshmallow
@@ -20,8 +19,13 @@ app = create_app()
 
 # Config
 
-app.config['SQLALCHEMY_DATABASE_URI'] = config.DATABASE_URI
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Using a production configuration
+
+# app.config.from_object('config.ProdConfig')
+
+# Using a development configuration
+
+app.config.from_object(config['dev'])
 
 # Init DB
 
@@ -39,4 +43,4 @@ app.register_blueprint(general)
 
 if __name__ == '__main__':
 
-    app.run(host=config.IP_SERVER, port=config.PORT_SERVER, debug=config.DEBUG)
+    app.run(host=app.config["HOST"], port=app.config["PORT"])
