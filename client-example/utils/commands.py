2c9cc6b9329bdf854c7f748f08de86104c4a90eb
ram <dementor20@protonmail.com>
2022-02-28 16:09:32 -0300

    Ejemplo de cliente eliminado

--- client-example/utils/commands.py
+++
@@ -1,8 +0,0 @@
-
-import subprocess
-
-def get_output(cmd):
-
-    sp = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-
-    return (sp.stdout.read()).decode("utf-8")
